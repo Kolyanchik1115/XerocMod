ccd7922e016a3a2eaa35b97a3fad73c63bfaa24f
Sprat <105858796+Kolyanchik1115@users.noreply.github.com>
2022-09-30 21:13:32 +0300

    CollegeJoke(add kurs,diploma and snickers)

--- src/main/java/net/javamod/xeroc/item/ModItems.java
+++ src/main/java/net/javamod/xeroc/item/ModItems.java
@@ -7,7 +7,6 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-
 import static net.javamod.xeroc.Xeroc.MOD_ID;
 
 public class ModItems {
